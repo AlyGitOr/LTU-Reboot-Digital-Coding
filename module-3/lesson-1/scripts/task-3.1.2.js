4a273d8bd73f33950ffb8213c2c60b873a4af7b0
AlyGitOr <77690419+AlyGitOr@users.noreply.github.com>
2021-02-11 20:39:02 +0000

    Update task-3.1.2.js

--- module-3/lesson-1/scripts/task-3.1.2.js
+++ module-3/lesson-1/scripts/task-3.1.2.js
@@ -1,6 +1,8 @@
 console.log('This is a message');
+
 alert('This is a message - 2nd');
-document.wwrite('This is a 3rd message');
+
+document.write('This is a 3rd message');
 
 //You can use the code below - watch 20:26 onwards Th 11/02/21
-document.querySelector('body').append();
+//document.querySelector('body').append();
