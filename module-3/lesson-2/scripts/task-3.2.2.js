765d0d4ad575ab93cc1cd0d30676e11b90d30147
AlyGitOr <77690419+AlyGitOr@users.noreply.github.com>
2021-02-16 19:42:43 +0000

    Completed 3.2.2 - Functions & Arguments

---
+++ module-3/lesson-2/scripts/task-3.2.2.js
@@ -0,0 +1,14 @@
+function getFullName(firstName, lastName){
+    let fullName = firstName + ' ' + lastName;
+    console.log(fullName);
+
+}
+
+var fName = "Mary";
+var lName = "Smith";
+
+var fName2 = "Phil";
+var lName2 = "Barlow";
+
+getFullName(fName,lName);
+getFullName(fName2, lName2);
